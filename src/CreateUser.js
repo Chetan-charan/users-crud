3d416840196e153e89f7ef3245a8d76003e5e927
Chetan-charan <chetanharicharan@gmail.com>
2021-11-17 21:45:42 +0530

    validations

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,35 +1,80 @@
-import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const validationForm = yup.object({                                                      //validation object for returning all errors
+  Name: yup.string().min(4,'Minimum 4 characters required!!').required('required'),
+  age: yup.number().min(1).max(100).required('required'),
+  city: yup.string().min(4,'Minimum 4 characters required!!').required('required'),
+  profilePic: yup.string().min(4,'Minimum 4 characters required 😄').required('required'),
+
+})
 
 //component for creating user with the details and adding the values to all users list 
-export function CreateUser({ users, setUsers }) {
+export function CreateUser() {
   const history = useHistory();
-  const [Name, setName] = useState();
-  const [age, setAge] = useState();
-  const [city, setCity] = useState();
-  const [profilePic, setpic] = useState();     
-  
-  return <div className='input-fields'>
+ 
 
-    <TextField onChange={(event) => setName(event.target.value)} id="standard-basic" label="Name" variant="standard" />
-    <TextField onChange={(event) => setAge(event.target.value)} id="standard-basic" label="Age" variant="standard" />
-    <TextField onChange={(event) => setCity(event.target.value)} id="standard-basic" label="City" variant="standard" />
-    <TextField onChange={(event) => setpic(event.target.value)} id="standard-basic" label="pic url" variant="standard" />
-    <Button onClick={() => {
-      const newUser = { Name, age, city, profilePic };
+  const {handleSubmit,handleBlur,handleChange,errors,values,touched} = useFormik({     //formik object used to validate form values
+    initialValues: {
+      Name: '',age: '',city: '',profilePic: '',
+    },
+    validationSchema: validationForm,
+    onSubmit: (values) => {                                                          //once validation complete, POSt request called through onsubmit
+    
       fetch('https://6166c4e013aa1d00170a670a.mockapi.io/usersInfo',
       {
-        method: 'POST',body: JSON.stringify(newUser),
+        method: 'POST',body: JSON.stringify(values),
         headers: {
         'Content-Type': 'application/json'
       },})
       .then(() =>   history.push('/users'));
-      
-    
-    }} variant="contained">Add user</Button>
-  </div>;
+    }
+  })
+
+
+
+  return <form onSubmit={handleSubmit}><div className='input-fields'>
+
+    <TextField name='Name' 
+    onChange={handleChange}      
+    helperText={errors.Name && touched.Name && errors.Name} 
+    value={values.Name} 
+    onBlur={handleBlur} 
+    error={errors.Name && touched.Name}
+    id="standard-basic" label="Name" variant="standard" />
+
+    <TextField name='age' 
+    onChange={handleChange}       
+    helperText={errors.age && touched.age && errors.age} 
+    value={values.age} 
+    onBlur={handleBlur} 
+    error={errors.age && touched.age}
+    id="standard-basic" label="Age" variant="standard" />
+
+    <TextField name='city' 
+    onChange={handleChange}      
+    helperText={errors.city && touched.city && errors.city} 
+    value={values.city} 
+    error={errors.city && touched.city}
+    onBlur={handleBlur} 
+    id="standard-basic" label="City" variant="standard" />
+
+    <TextField name='profilePic' 
+    onChange={handleChange} 
+    helperText={errors.profilePic && touched.profilePic && errors.profilePic} 
+    value={values.profilePic} 
+    error={errors.profilePic && touched.profilePic}
+    onBlur={handleBlur} 
+    id="standard-basic" label="pic url" variant="standard" />
+
+    <Button type='submit' variant="contained">Add user</Button>
+  </div>
+  </form>
 
 
 }
+
+
--- src/EditUser.js
+++ src/EditUser.js
@@ -3,45 +3,91 @@ import { useParams } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const validationForm = yup.object({
+  Name: yup.string().min(4,'Minimum 4 characters required!!').required('required'),
+  age: yup.number().min(1).max(100).required('required'),
+  city: yup.string().min(4,'Minimum 4 characters required!!').required('required'),
+  profilePic: yup.string().min(4,'Minimum 4 characters required 😄').required('required'),
+
+})
 
  //Component to Edit the user selected
 export function EditUser() {        
-  const history = useHistory();
+  
   const { id } = useParams();
-  const [Name, setName] = useState('');           
-  const [age, setAge] = useState('');
-  const [city, setCity] = useState('');
-  const [profilePic, setpic] = useState('');
-
+  const [editUser,setEditUser] = useState(null);
   useEffect(() => {
     fetch(`https://6166c4e013aa1d00170a670a.mockapi.io/usersInfo/${id}`,{method: 'GET'})   //get particular user data and fill the text fields with the values
     .then((data) => data.json())
-    .then((user) => {
-        setName(user.Name);
-        setAge(user.age);
-        setCity(user.city);
-        setpic(user.profilePic);
-    })
-  },[])
+    .then((user) => setEditUser(user))
+  },[id])
 
-  const styles = {
-    display: 'flex', flexDirection: 'column', gap: '10px', width: '40%', margin: '25px'    //some styles used
-  };
+  return editUser ? <UpdateUser editUser={editUser}/> : ''    //if edituser was fetched, rendering update user component
 
-  //changing values as per the entry
-  return <div style={styles}>
-    <TextField value={Name} onChange={(event) => setName(event.target.value)} id="standard-basic" label="Name" variant="standard" />
-    <TextField value={age} onChange={(event) => setAge(event.target.value)} id="standard-basic" label="Age" variant="standard" />
-    <TextField value={city} onChange={(event) => setCity(event.target.value)} id="standard-basic" label="City" variant="standard" />
-    <TextField value={profilePic} onChange={(event) => setpic(event.target.value)} id="standard-basic" label="Profile Pic url" variant="standard" />
-    <Button onClick={() => {
-      const editUser = {Name,age,city,profilePic};                          //new values updated on change
-      fetch(`https://6166c4e013aa1d00170a670a.mockapi.io/usersInfo/${id}`,
-      {method: 'PUT',headers: {
-        'Content-type': 'application/json'
-      },body: JSON.stringify(editUser)}
-      ).then(() =>  history.push('/users'));                                 //on button click new values updated to the user and path set back to all users
+}
 
-    }} variant="outlined">Save User</Button>
-  </div>;                                      
+
+function UpdateUser({editUser}){
+  const history = useHistory();
+  const { id } = useParams();
+
+  const {handleSubmit,handleBlur,handleChange,errors,values,touched} = useFormik({      //using formik to edit users
+    initialValues: {
+      Name: editUser.Name,age: editUser.age ,city: editUser.city ,profilePic: editUser.profilePic,
+    },
+    validationSchema: validationForm,
+    onSubmit: (values) => {
+      
+      fetch(`https://6166c4e013aa1d00170a670a.mockapi.io/usersInfo/${id}`, 
+      {
+        method: 'PUT',body: JSON.stringify(values),                                 //if validation complete, onsubmit put method is called and values object passed
+        headers: {
+        'Content-Type': 'application/json'
+      },})
+      .then(() =>   history.push('/users'));                         //once completed put method, return to users list
+    }
+  })
+ //formik object destructured to handle form
+  return <form onSubmit={handleSubmit}>                   
+        <div className='input-fields'>
+        <TextField name='Name' 
+        onChange={handleChange}      
+        helperText={errors.Name && touched.Name && errors.Name} 
+        value={values.Name} 
+        onBlur={handleBlur} 
+        error={errors.Name && touched.Name}
+        id="standard-basic" label="Name" variant="standard" />
+
+        <TextField name='age' 
+        onChange={handleChange}       
+        helperText={errors.age && touched.age && errors.age} 
+        value={values.age} 
+        onBlur={handleBlur} 
+        error={errors.age && touched.age}
+        id="standard-basic" label="Age" variant="standard" />
+
+        <TextField name='city' 
+        onChange={handleChange}      
+        helperText={errors.city && touched.city && errors.city} 
+        value={values.city} 
+        error={errors.city && touched.city}
+        onBlur={handleBlur} 
+        id="standard-basic" label="City" variant="standard" />
+
+        <TextField name='profilePic' 
+        onChange={handleChange} 
+        helperText={errors.profilePic && touched.profilePic && errors.profilePic} 
+        value={values.profilePic} 
+        error={errors.profilePic && touched.profilePic}
+        onBlur={handleBlur} 
+        id="standard-basic" label="pic url" variant="standard" />
+
+        <Button type='submit' variant="contained">Save User</Button>
+      </div>
+      </form>
+
+
 }
